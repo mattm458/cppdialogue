16341e67fe9ae3779a320f30bc3f5d735bf63f5a
Matt McNeill <mattm458@gmail.com>
2025-06-03 15:36:40 -0400

    LowerCaseProcessor transforms incoming strings into lower case

--- src/PipelineExample/LowerCaseProcessor.cpp
+++ src/PipelineExample/LowerCaseProcessor.cpp
@@ -1,6 +1,16 @@
 #include <PipelineExample/LowerCaseProcessor.h>
 
+#include <algorithm>
+#include <string>
+
 LowerCaseProcessor::LowerCaseProcessor()
     : Processor<std::string, std::string>() {};
 
-std::string LowerCaseProcessor::process(std::string &in) { return in; }
+std::string LowerCaseProcessor::process(std::string &in) {
+    std::string out = in;
+
+    std::transform(out.begin(), out.end(), out.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
+
+    return out;
+}
